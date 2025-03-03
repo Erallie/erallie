43b78b026fe857bd457bcb795929f4d40d332e2f
Erallie <31375589+Erallie@users.noreply.github.com>
2025-03-02 17:22:01 -0700

    Added some project pages

---
+++ src/routes/(projects)/discord/voicely-ping/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Readme from "$lib/components/Readme.svelte";
+	import Banner from "../../../Banner.svelte";
+</script>
+
+<Banner><h1>Voicely Ping</h1></Banner>
+
+<Readme repo="voicely-ping" branch="main" />
---
+++ src/routes/(projects)/obsidian/diarian/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Readme from "$lib/components/Readme.svelte";
+	import Banner from "../../../Banner.svelte";
+</script>
+
+<Banner><h1>Diarian</h1></Banner>
+
+<Readme repo="diarian" branch="master" />
