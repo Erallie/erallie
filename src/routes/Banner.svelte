5b6ec0655ff57c8c59fb954479dd28f94e20b414
Erallie <31375589+Erallie@users.noreply.github.com>
2025-03-02 17:20:29 -0700

    Added Banner.svelte

---
+++ src/routes/Banner.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<section id="banner">
+	{@render children()}
+</section>
+
+<style>
+	section {
+		text-align: right;
+		height: 200px;
+		display: flex;
+		justify-content: center;
+		background-image: url("/code-background-7.jpg");
+		/* background-attachment: fixed; */
+		background-size: cover; /* Cover the entire viewport */
+		background-position: top; /* Center the image */
+		background-repeat: no-repeat; /* Do not repeat the image */
+		/* text-shadow: 0 0 1em black; */
+	}
+</style>
