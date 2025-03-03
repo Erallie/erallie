538552ee6e11643b59014d3385b90f93efc371ff
Erallie <31375589+Erallie@users.noreply.github.com>
2025-03-02 17:21:05 -0700

    Put content in homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Banner from "./Banner.svelte";
+</script>
+
+<Banner>
+	<hgroup>
+		<h1>We make stuff.</h1>
+		<h1><strong>LOTS</strong> of stuff.</h1>
+	</hgroup>
+</Banner>
+<section>
+	<h1>What we do</h1>
+	<hr />
+	<p>
+		We code a wide variety of tools, ranging from Discord Bots to Obsidian
+		plugins to VR aids. Our areas of focus expand as we are involved in new
+		online interests. We aim to make our projects free for all to use.
+	</p>
+</section>
+
+<style>
+	section {
+		background-color: rgba(0, 0, 0, 0.2);
+		border-radius: 40px;
+		margin: 40px auto;
+		width: 500px;
+		padding: 40px;
+	}
+	hr {
+		width: 100px;
+		margin: 1em 0px;
+		margin-left: 2px;
+		padding: 0px;
+	}
+</style>
