9a728431446a8c7fd7fc283afc5d01400f9880ac
Erallie <31375589+Erallie@users.noreply.github.com>
2025-03-02 17:23:24 -0700

    Added temporary Section.svelte

---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    let { title, children } = $props();
+</script>
+
+<section id={title.toLowerCase().replaceAll(" ", "-").replaceAll("'", "")}>
+    <h1>{title}</h1>
+    <hr />
+    {@render children()}
+</section>
+
+<style>
+    section {
+        background-color: rgba(0, 0, 0, 0.2);
+        border-radius: 40px;
+        margin: 40px auto;
+        width: 500px;
+        padding: 40px;
+    }
+    hr {
+        width: 100px;
+        margin: 1em 0px;
+        margin-left: 2px;
+        padding: 0px;
+    }
+</style>
