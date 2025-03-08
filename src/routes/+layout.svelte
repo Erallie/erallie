c926ac1a3fd0f9df838cf1dd88a00c698bcfbfbd
Erallie <erika@gozarproductions.com>
2025-03-08 15:50:36 -0700

    Prettified some files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,91 +1,91 @@
 <script lang="ts">
-    import Header from "./Header.svelte";
-    import logo from "$lib/images/gozar-productions-logo.svg";
-    import "../app.css";
+	import Header from "./Header.svelte";
+	import logo from "$lib/images/gozar-productions-logo.svg";
+	import "../app.css";
 
-    let { children } = $props();
+	let { children } = $props();
 </script>
 
 <div class="app">
-    <Header />
+	<Header />
 
-    <main>
-        {@render children()}
-    </main>
+	<main>
+		{@render children()}
+	</main>
 
-    <footer>
-        <h1>
-            <img src={logo} alt="Gozar Productions Logo" />Gozar Productions
-        </h1>
-        <div>
-            <a href="/privacy-policy">Privacy Policy</a>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="100"
-                height="100"
-                viewBox="0 0 100 100"
-            >
-                <circle cx="50" cy="50" r="10" fill="currentColor" />
-            </svg>
-            <a href="/disclaimer">Disclaimer</a>
-        </div>
-        <p>© 2025 Gozar Productions LLC. All Rights Reserved.</p>
-    </footer>
+	<footer>
+		<h1>
+			<img src={logo} alt="Gozar Productions Logo" />Gozar Productions
+		</h1>
+		<div>
+			<a href="/privacy-policy">Privacy Policy</a>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="100"
+				height="100"
+				viewBox="0 0 100 100"
+			>
+				<circle cx="50" cy="50" r="10" fill="currentColor" />
+			</svg>
+			<a href="/disclaimer">Disclaimer</a>
+		</div>
+		<p>© 2025 Gozar Productions LLC. All Rights Reserved.</p>
+	</footer>
 </div>
 
 <style>
-    .app {
-        display: flex;
-        flex-direction: column;
-        min-height: 100dvh;
-    }
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100dvh;
+	}
 
-    main {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-        /* max-width: 64rem; */
-        /* margin: 0 auto; */
-        box-sizing: border-box;
-    }
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		/* max-width: 64rem; */
+		/* margin: 0 auto; */
+		box-sizing: border-box;
+	}
 
-    footer {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        padding: 12px;
-        background-color: black;
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+		background-color: black;
 
-        color: white;
-        font-weight: 300;
-        text-align: center;
-        & img {
-            aspect-ratio: 1 / 1;
-            height: 4rem;
-            display: block;
-            margin: 12px auto;
-        }
-        & h1 {
-            font-size: 1.5rem;
-            font-weight: 300;
-            line-height: 1.1em;
-        }
-        & svg {
-            aspect-ratio: 1 / 1;
-            width: auto;
-            height: 1em;
-            vertical-align: middle;
-        }
-        & p {
-            line-height: 1.2em;
-        }
-        & p,
-        & a {
-            color: inherit;
-            font-weight: inherit;
-            font-size: 0.85rem;
-        }
-    }
+		color: white;
+		font-weight: 300;
+		text-align: center;
+		& img {
+			aspect-ratio: 1 / 1;
+			height: 4rem;
+			display: block;
+			margin: 12px auto;
+		}
+		& h1 {
+			font-size: 1.5rem;
+			font-weight: 300;
+			line-height: 1.1em;
+		}
+		& svg {
+			aspect-ratio: 1 / 1;
+			width: auto;
+			height: 1em;
+			vertical-align: middle;
+		}
+		& p {
+			line-height: 1.2em;
+		}
+		& p,
+		& a {
+			color: inherit;
+			font-weight: inherit;
+			font-size: 0.85rem;
+		}
+	}
 </style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-    import logo from "$lib/images/gozar-productions-logo.svg";
+	import logo from "$lib/images/gozar-productions-logo.svg";
 </script>
 
 <header>
-    <hgroup>
-        <div>
-            <h1>Gozar Productions</h1>
-            <h2>Code</h2>
-        </div>
-        <img src={logo} alt="Gozar Productions Logo" />
-    </hgroup>
-    <hr class="vertical-rule" />
-    <nav>
-        <ul>
-            <li>
-                <a href="/">Home</a>
-            </li>
-            <li>
-                <a href="/projects">Our Projects</a>
-                <ul>
+	<hgroup>
+		<div>
+			<h1>Gozar Productions</h1>
+			<h2>Code</h2>
+		</div>
+		<img src={logo} alt="Gozar Productions Logo" />
+	</hgroup>
+	<hr class="vertical-rule" />
+	<nav>
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/projects">Our Projects</a>
+				<!-- <ul>
                     <li>
                         <a href="/obsidian">Obsidian</a>
                         <ul>
@@ -41,132 +41,132 @@
                             </li>
                         </ul>
                     </li>
-                </ul>
-            </li>
-            <li>
-                <a href="#contact">Contact</a>
-            </li>
-            <li>
-                <a href="https://www.gozarproductions.com">Back</a>
-            </li>
-        </ul>
-    </nav>
+                </ul> -->
+			</li>
+			<li>
+				<a href="/#contact">Contact</a>
+			</li>
+			<li>
+				<a href="https://www.gozarproductions.com">Back</a>
+			</li>
+		</ul>
+	</nav>
 </header>
 <section></section>
 
 <style>
-    hgroup {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: max-content;
-        padding: 10px;
-        margin: 0px;
-        transition: background-color 0.3s;
-        & > img {
-            aspect-ratio: 1 / 1;
-            height: 4em;
-            vertical-align: middle;
-        }
-        & h1,
-        & h2 {
-            text-align: right;
-            padding: 0px;
-            margin: 0px;
-        }
-        & > :first-child {
-            margin: 0px;
-            margin-right: 0.4em;
-        }
-        & > :last-child {
-            margin: 0px;
-            margin-left: 0.4em;
-        }
-    }
+	hgroup {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: max-content;
+		padding: 10px;
+		margin: 0px;
+		transition: background-color 0.3s;
+		& > img {
+			aspect-ratio: 1 / 1;
+			height: 4em;
+			vertical-align: middle;
+		}
+		& h1,
+		& h2 {
+			text-align: right;
+			padding: 0px;
+			margin: 0px;
+		}
+		& > :first-child {
+			margin: 0px;
+			margin-right: 0.4em;
+		}
+		& > :last-child {
+			margin: 0px;
+			margin-left: 0.4em;
+		}
+	}
 
-    header {
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        background-color: hsl(245, 37%, 17%);
-        position: fixed;
-        top: 0;
-        left: 0;
-    }
+	header {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+		background-color: hsl(245, 37%, 17%);
+		position: fixed;
+		top: 0;
+		left: 0;
+	}
 
-    nav {
-        display: flex;
-        align-items: center; /* Center items vertically */
-        & a {
-            color: inherit;
-            font-weight: initial;
-            display: flex;
-            align-items: center;
-            padding: 6px 10px;
-            &:hover {
-                text-decoration: none;
-            }
-        }
-        & ul {
-            list-style-type: none;
-            padding: 0px;
-        }
-        & > ul {
-            height: 100%;
-            display: flex;
-            & li {
-                text-align: center;
-                position: relative;
-                width: max-content;
-                &:hover {
-                    & > a {
-                        background-color: rgba(0, 0, 0, 0.2);
-                        /* color: black; */
-                    }
-                    & > ul {
-                        display: block; /* Show nested ul on hover */
-                    }
-                }
-            }
-            & > li {
-                height: 100%;
-                box-sizing: border-box;
-                & > a {
-                    height: 100%;
-                    box-sizing: border-box;
-                }
-                & ul {
-                    display: none; /* Hide nested ul by default */
-                    position: absolute; /* Position it absolutely */
-                    background-color: hsl(245, 37%, 17%);
-                    & li {
-                        width: max-content;
-                        min-width: 100%;
-                        margin: auto;
-                        box-sizing: border-box;
-                    }
-                }
-                & > ul {
-                    left: 0; /* Align to the left of the parent li */
-                    top: 100%; /* Position it below the parent li */
-                    & ul {
-                        right: 100%;
-                        top: 0;
-                    }
-                }
-            }
-        }
-    }
+	nav {
+		display: flex;
+		align-items: center; /* Center items vertically */
+		& a {
+			color: inherit;
+			font-weight: initial;
+			display: flex;
+			align-items: center;
+			padding: 6px 10px;
+			&:hover {
+				text-decoration: none;
+			}
+		}
+		& ul {
+			list-style-type: none;
+			padding: 0px;
+		}
+		& > ul {
+			height: 100%;
+			display: flex;
+			& li {
+				text-align: center;
+				position: relative;
+				width: max-content;
+				&:hover {
+					& > a {
+						background-color: rgba(0, 0, 0, 0.2);
+						/* color: black; */
+					}
+					& > ul {
+						display: block; /* Show nested ul on hover */
+					}
+				}
+			}
+			& > li {
+				height: 100%;
+				box-sizing: border-box;
+				& > a {
+					height: 100%;
+					box-sizing: border-box;
+				}
+				& ul {
+					display: none; /* Hide nested ul by default */
+					position: absolute; /* Position it absolutely */
+					background-color: hsl(245, 37%, 17%);
+					& li {
+						width: max-content;
+						min-width: 100%;
+						margin: auto;
+						box-sizing: border-box;
+					}
+				}
+				& > ul {
+					left: 0; /* Align to the left of the parent li */
+					top: 100%; /* Position it below the parent li */
+					& ul {
+						right: 100%;
+						top: 0;
+					}
+				}
+			}
+		}
+	}
 
-    hr.vertical-rule {
-        border: none;
-        border-left: 1px solid rgba(255, 255, 255, 0.7); /* Change color and width as needed */
-        height: 48px; /* Set the height of the line */
-        margin: auto 20px; /* Optional: add some margin */
-    }
+	hr.vertical-rule {
+		border: none;
+		border-left: 1px solid rgba(255, 255, 255, 0.7); /* Change color and width as needed */
+		height: 48px; /* Set the height of the line */
+		margin: auto 20px; /* Optional: add some margin */
+	}
 
-    section {
-        height: 84px;
-        background-color: black;
-    }
+	section {
+		height: 84px;
+		background-color: black;
+	}
 </style>
